0a09b8d06ac7411fc96cd7079f05a2d79a05d822
OrionAF <einar95@yahoo.com>
2025-09-21 02:54:17 +0000

    Add extensive custom hotspot preparations for various locations

--- data/Map.Hotspots.lua
+++ data/Map.Hotspots.lua
@@ -1,6 +1,239 @@
 setfenv(1, GoggleMaps)
 
 GoggleMaps.CustomHotspots = {
+  -- Kalimdor
+  [1000] = "",
+  -- Ashenvale
+  [1001] = "",
+  -- Azshara
+  [1002] = "",
+  -- Blackstone Island
+  [1003] = "",
+  -- Caverns of Time
+  [1004] = "",
+  -- Darkshore
+  [1005] = "",
+  -- Darnassus
+  [1006] = "",
+  -- Desolace
+  [1007] = "",
+  -- Dire Maul
+  [1008] = "",
+  -- Durotar
+  [1009] = "",
+  -- Dustwallow Marsh
+  [1010] = "",
+  -- Felwood
+  [1011] = "",
+  -- Feralas
+  [1012] = "",
+  -- Gates of Ahn'Qiraj
+  [1013] = "",
+  -- GM Island
+  [1014] = "",
+  -- Hyjal
+  [1015] = "",
+  -- Icepoint Rock
+  [1016] = "",
+  -- Maraudon
+  [1017] = "",
+  -- Moonglade
+  [1018] = "",
+  -- Mulgore
+  [1019] = "",
+  -- Orgrimmar
+  [1020] = "",
+  -- Silithus
+  [1021] = "",
+  -- Stonetalon Mountains
+  [1022] = "",
+  -- Tanaris
   [1023] = "33.81^2.05^4.00^1.00^CUSTOM~32.81^3.05^6.00^1.00^CUSTOM~35.81^4.05^3.00^1.00^CUSTOM~32.81^5.05^7.00^2.00^CUSTOM~31.81^7.05^9.00^8.00^CUSTOM~32.81^15.05^8.00^3.00^CUSTOM~61.81^15.05^3.00^1.00^CUSTOM~40.81^16.05^1.00^68.00^CUSTOM~59.81^16.05^6.00^1.00^CUSTOM~41.81^17.05^1.00^67.00^CUSTOM~58.81^17.05^7.00^1.00^CUSTOM~33.81^18.05^7.00^3.00^CUSTOM~56.81^18.05^10.00^1.00^CUSTOM~42.81^19.05^2.00^64.00^CUSTOM~53.81^19.05^13.00^1.00^CUSTOM~44.81^20.05^22.00^37.00^CUSTOM~34.81^21.05^6.00^4.00^CUSTOM~66.81^21.05^1.00^12.00^CUSTOM~35.81^25.05^5.00^7.00^CUSTOM~34.81^32.05^6.00^4.00^CUSTOM~33.81^36.05^7.00^3.00^CUSTOM~66.81^38.05^1.00^18.00^CUSTOM~32.81^39.05^8.00^2.00^CUSTOM~67.81^40.05^1.00^15.00^CUSTOM~31.81^41.05^9.00^2.00^CUSTOM~68.81^41.05^1.00^14.00^CUSTOM~69.81^42.05^1.00^12.00^CUSTOM~30.81^43.05^10.00^2.00^CUSTOM~70.81^43.05^1.00^10.00^CUSTOM~71.81^44.05^2.00^7.00^CUSTOM~29.81^45.05^11.00^2.00^CUSTOM~73.81^45.05^1.00^4.00^CUSTOM~28.81^47.05^12.00^2.00^CUSTOM~27.81^49.05^13.00^2.00^CUSTOM~26.81^51.05^14.00^2.00^CUSTOM~71.81^51.05^1.00^1.00^CUSTOM~25.81^53.05^15.00^3.00^CUSTOM~24.81^56.05^16.00^5.00^CUSTOM~44.81^57.05^20.00^2.00^CUSTOM~44.81^59.05^19.00^2.00^CUSTOM~23.81^61.05^17.00^9.00^CUSTOM~44.81^61.05^18.00^9.00^CUSTOM~24.81^70.05^16.00^3.00^CUSTOM~44.81^70.05^17.00^5.00^CUSTOM~25.81^73.05^15.00^2.00^CUSTOM~26.81^75.05^14.00^1.00^CUSTOM~44.81^75.05^16.00^1.00^CUSTOM~27.81^76.05^13.00^1.00^CUSTOM~44.81^76.05^15.00^1.00^CUSTOM~28.81^77.05^12.00^2.00^CUSTOM~44.81^77.05^14.00^1.00^CUSTOM~44.81^78.05^13.00^1.00^CUSTOM~29.81^79.05^11.00^1.00^CUSTOM~44.81^79.05^12.00^1.00^CUSTOM~30.81^80.05^10.00^2.00^CUSTOM~44.81^80.05^11.00^3.00^CUSTOM~32.81^82.05^8.00^1.00^CUSTOM~34.81^83.05^6.00^1.00^CUSTOM~42.81^83.05^1.00^1.00^CUSTOM~47.81^83.05^7.00^1.00^CUSTOM~48.81^84.05^6.00^1.00^CUSTOM~50.81^85.05^3.00^2.00^CUSTOM",
+  -- Tel'Abim
+  [1024] = "",
+  -- Teldrassil
+  [1025] = "",
+  -- The Barrens
+  [1026] = "",
+  -- Thousand Needles
+  [1027] = "",
+  -- Thunder Bluff
+  [1028] = "",
+  -- Un'Goro Crater
+  [1029] = "",
+  -- Wailing Caverns
+  [1030] = "",
+  -- Winterspring
+  [1031] = "",
+  -- Eastern Kingdoms
+  [2000] = "",
+  -- Alah'Thalas
+  [2001] = "",
+  -- Alterac Mountains
+  [2002] = "",
+  -- Arathi Highlands
+  [2003] = "",
+  -- Badlands
+  [2004] = "",
+  -- Balor
+  [2005] = "",
+  -- Blackrock Mountain
+  [2006] = "",
+  -- Blasted Lands
+  [2007] = "",
+  -- Burning Steppes
+  [2008] = "",
+  -- Deadwind Pass
+  [2009] = "",
+  -- Dun Morogh
+  [2010] = "",
+  -- Duskwood
+  [2011] = "",
+  -- Eastern Plaguelands
+  [2012] = "",
+  -- Elwynn Forest
+  [2013] = "",
+  -- Gillijim's Isle
+  [2014] = "",
+  -- Gilneas
+  [2015] = "",
+  -- Gnomeregan
+  [2016] = "",
+  -- Grim Reaches
+  [2017] = "",
+  -- Hillsbrad Foothills
+  [2018] = "",
+  -- Ironforge
+  [2019] = "",
+  -- Lapidis Isle
+  [2020] = "",
+  -- Loch Modan
+  [2021] = "",
+  -- Northwind
+  [2022] = "",
+  -- Redridge Mountains
+  [2023] = "",
+  -- Scarlet Enclave
+  [2024] = "",
+  -- Scarlet Monastery
+  [2025] = "",
+  -- Searing Gorge
+  [2026] = "",
+  -- Silverpine Forest
+  [2027] = "",
+  -- Stormwind City
+  [2028] = "",
+  -- Stranglethorn Vale
+  [2029] = "",
+  -- Swamp of Sorrows
+  [2030] = "",
+  -- Thallassian Highlands
+  [2031] = "",
+  -- The Deadmines
+  [2032] = "",
+  -- The Hinterlands
+  [2033] = "",
+  -- Tirisfal Glades
+  [2034] = "",
+  -- Uldaman
+  [2035] = "",
+  -- Undercity
+  [2036] = "",
+  -- Western Plaguelands
+  [2037] = "",
+  -- Westfall
+  [2038] = "",
+  -- Wetlands
+  [2039] = "",
+  -- Blackfathom Deeps
+  [50001] = "",
+  -- Blackrock Depths
+  [50002] = "",
+  -- Blackrock Spire
+  [50003] = "",
+  -- Crescent Grove
+  [50004] = "",
+  -- Dire Mail
+  [50005] = "",
+  -- Dragonmaw Retreat
+  [50006] = "",
+  -- Gilneas City
+  [50007] = "",
+  -- Gnomeregan
+  [50008] = "",
+  -- Hateforge Quarry
+  [50009] = "",
+  -- Karazhan Crypt
+  [50010] = "",
+  -- Maraudon
+  [50011] = "",
+  -- Ragefire Chasm
+  [50012] = "",
+  -- Razorfen Downs
+  [50013] = "",
+  -- Razorfen Kraul
+  [50014] = "",
+  -- Scarlet Monastery Armory
+  [50015] = "",
+  -- Scarlet Monastery Cathedral
+  [50016] = "",
+  -- Scarlet Monastery Graveyard
+  [50017] = "",
+  -- Scarlet Monastery Library
+  [50018] = "",
+  -- Scholomance
+  [50019] = "",
+  -- Shadowfang Keep
+  [50020] = "",
+  -- Stormwind Vault
+  [50021] = "",
+  -- Stratholme
+  [50022] = "",
+  -- The Black Morass
+  [50023] = "",
+  -- The Black Morass 2
+  [50024] = "",
+  -- The Deadmines
+  [50025] = "",
+  -- The Stockade
+  [50026] = "",
+  -- The Temple of Atal'Hakkar
+  [50027] = "",
+  -- Uldaman
+  [50028] = "",
+  -- Wailing Caverns
+  [50029] = "",
+  -- Zul'Farrak
+  [50030] = "",
+  -- Ahn'Qiraj
+  [60001] = "",
+  -- Ahn'Qiraj 2
+  [60002] = "",
+  -- Blackwing Lair
+  [60003] = "",
+  -- Emerald Sanctum
+  [60004] = "",
+  -- Molten Core
+  [60005] = "",
+  -- Naxxramas
+  [60006] = "",
+  -- Onyxia's Lair
+  [60007] = "",
+  -- Ruins of Ahn'Qiraj
+  [60008] = "",
+  -- The Upper Necropolis
+  [60009] = "",
+  -- Tower of Karazhan
+  [60010] = "",
+  -- Tower of Karazhan 2
+  [60011] = "",
+  -- Zul'Gurub
+  [60012] = "",
+  -- Alterac Valley
+  [70001] = "",
+  -- Arathi Basin
+  [70002] = "",
+  -- Warsong Gulch
+  [70003] = ""
 }
 
